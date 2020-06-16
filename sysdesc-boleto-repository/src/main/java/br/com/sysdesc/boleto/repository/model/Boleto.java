8f698e07e4e760bbe4c8f61d4d5c9d363992e1cb
leandrozanatta@sysmo.com.br <4fjJKS#2>
2020-06-16 14:53:45 -0300

    Doc: Removido campo de envio de emails

--- sysdesc-boleto-repository/src/main/java/br/com/sysdesc/boleto/repository/model/Boleto.java
+++ sysdesc-boleto-repository/src/main/java/br/com/sysdesc/boleto/repository/model/Boleto.java
@@ -27,62 +27,59 @@ import lombok.Setter;
 @Table(name = "tb_boleto")
 @SequenceGenerator(name = "GEN_BOLETO", allocationSize = 1, sequenceName = "GEN_BOLETO")
 public class Boleto implements Serializable {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "GEN_BOLETO")
-    @Column(name = "id_boleto")
-    private Long idBoleto;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "GEN_BOLETO")
+	@Column(name = "id_boleto")
+	private Long idBoleto;
 
-    @ManyToOne
-    @JoinColumn(name = "cd_configuracaoboleto")
-    private ConfiguracaoBoleto configuracaoBoleto;
+	@ManyToOne
+	@JoinColumn(name = "cd_configuracaoboleto")
+	private ConfiguracaoBoleto configuracaoBoleto;
 
-    @Column(name = "fl_aceite")
-    private String aceite;
+	@Column(name = "fl_aceite")
+	private String aceite;
 
-    @Column(name = "nr_especietitulo")
-    private Long especieTitulo;
+	@Column(name = "nr_especietitulo")
+	private Long especieTitulo;
 
-    @Column(name = "cd_numerobanco")
-    private Long numeroBanco;
+	@Column(name = "cd_numerobanco")
+	private Long numeroBanco;
 
-    @Column(name = "cd_nossonumero")
-    private String nossoNumero;
+	@Column(name = "cd_nossonumero")
+	private String nossoNumero;
 
-    @Column(name = "cd_codigobarras")
-    private String codigoBarras;
+	@Column(name = "cd_codigobarras")
+	private String codigoBarras;
 
-    @Column(name = "vl_boleto")
-    private BigDecimal valorBoleto;
+	@Column(name = "vl_boleto")
+	private BigDecimal valorBoleto;
 
-    @Column(name = "dt_vencimento")
-    @Temporal(TemporalType.DATE)
-    private Date dataVencimento;
+	@Column(name = "dt_vencimento")
+	@Temporal(TemporalType.DATE)
+	private Date dataVencimento;
 
-    @Column(name = "dt_cadastro")
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date dataCadastro;
+	@Column(name = "dt_cadastro")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataCadastro;
 
-    @Column(name = "bl_arquivo")
-    private byte[] arquivo;
+	@Column(name = "bl_arquivo")
+	private byte[] arquivo;
 
-    @Column(name = "cd_status")
-    private Long codigoStatus;
+	@Column(name = "cd_status")
+	private Long codigoStatus;
 
-    @Column(name = "cd_statusemail")
-    private Long codigoStatusEmail;
+	@OneToOne(mappedBy = "boleto", cascade = CascadeType.ALL)
+	private BoletoDadosCliente boletoDadosCliente;
 
-    @OneToOne(mappedBy = "boleto", cascade = CascadeType.ALL)
-    private BoletoDadosCliente boletoDadosCliente;
+	@OneToOne(mappedBy = "boleto", cascade = CascadeType.ALL)
+	private BoletoDadosSacadorAvalista boletoDadosSacadorAvalista;
 
-    @OneToOne(mappedBy = "boleto", cascade = CascadeType.ALL)
-    private BoletoDadosSacadorAvalista boletoDadosSacadorAvalista;
+	@OneToOne(mappedBy = "boleto", cascade = CascadeType.ALL)
+	private BoletoDadosPagamento boletoDadosPagamento;
 
-    @OneToOne(mappedBy = "boleto", cascade = CascadeType.ALL)
-    private BoletoDadosPagamento boletoDadosPagamento;
-
-    @OneToOne(mappedBy = "boleto")
-    private RemessaBoleto remessaBoleto;
+	@OneToOne(mappedBy = "boleto")
+	private RemessaBoleto remessaBoleto;
 
 }
