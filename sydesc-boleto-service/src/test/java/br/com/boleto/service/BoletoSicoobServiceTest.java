c06aa93b35bea11d7bee511f9f62c649e2a67d25
leandrozanatta@sysmo.com.br <4fjJKS#2>
2020-07-17 01:24:11 -0300

    Doc: Processamento de retorno de pagamentos

--- sydesc-boleto-service/src/test/java/br/com/boleto/service/BoletoSicoobServiceTest.java
+++ sydesc-boleto-service/src/test/java/br/com/boleto/service/BoletoSicoobServiceTest.java
@@ -14,33 +14,33 @@ import br.com.sysdesc.boletos.service.boleto.sicoob.BoletoSicoobService;
 @RunWith(MockitoJUnitRunner.class)
 public class BoletoSicoobServiceTest {
 
-    private BoletoSicoobService boletoSicoobService;
+	private BoletoSicoobService boletoSicoobService;
 
-    private ConfiguracaoBoleto configuracaoBoleto = new ConfiguracaoBoleto();
+	private ConfiguracaoBoleto configuracaoBoleto = new ConfiguracaoBoleto();
 
-    @Before
-    public void initialize() {
-        configuracaoBoleto.setCodigoCarteira(1L);
-        configuracaoBoleto.setModalidade("01");
+	@Before
+	public void initialize() {
+		configuracaoBoleto.setCodigoCarteira(1L);
+		configuracaoBoleto.setModalidade("01");
 
-        boletoSicoobService = Mockito.spy(new BoletoSicoobService(configuracaoBoleto));
-    }
+		boletoSicoobService = Mockito.spy(new BoletoSicoobService(configuracaoBoleto));
+	}
 
-    @Test
-    public void testarNossoNumero() throws Exception {
-        configuracaoBoleto.setNumeroAgencia("0001-4");
-        configuracaoBoleto.setNumeroConta("1-9");
-        configuracaoBoleto.setNossoNumero(21L);
+	@Test
+	public void testarNossoNumero() throws Exception {
+		configuracaoBoleto.setNumeroAgencia("0001-4");
+		configuracaoBoleto.setNumeroConta("1-9");
+		configuracaoBoleto.setNossoNumero(21L);
 
-        assertEquals("00000218", boletoSicoobService.gerarNossoNumero());
-    }
+		assertEquals("0000000218", boletoSicoobService.gerarNossoNumero());
+	}
 
-    @Test
-    public void testarNossoNumero2() throws Exception {
-        configuracaoBoleto.setNumeroAgencia("1001-4");
-        configuracaoBoleto.setNumeroConta("216844-5");
-        configuracaoBoleto.setNossoNumero(13L);
+	@Test
+	public void testarNossoNumero2() throws Exception {
+		configuracaoBoleto.setNumeroAgencia("1001-4");
+		configuracaoBoleto.setNumeroConta("216844-5");
+		configuracaoBoleto.setNossoNumero(13L);
 
-        assertEquals("00000130", boletoSicoobService.gerarNossoNumero());
-    }
+		assertEquals("0000000130", boletoSicoobService.gerarNossoNumero());
+	}
 }
